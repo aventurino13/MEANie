d7eeb64fb5df0f285accc6d6e6d3da5f6006e40b
Amy Venturino <aventuri@umich.edu>
2017-05-10 15:04:44 -0500

    First commit on clean-up branch

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,37 @@
-var myApp=angular.module( 'myApp', [] );
-myApp.controller( 'WhereMyPeeps', [ '$http', function( $http ){
-var vm = this;
-vm.addRecord = function(){
-var objectToSend ={
-name= vm.nameIn,
-location= vm.locationIn,
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-vm.nameIn ='';
-vm.locationIn='';
-};
+var myApp = angular.module( 'myApp', [] );
+
+myApp.controller( 'WhereMyPeeps', function( $http ){
+
+  var vm = this;
+
+ vm.addRecord = function(){
+    var objectToSend ={
+      name : vm.nameIn,
+      location : vm.locationIn,
+     };
+    $http({
+      method: 'POST',
+      url: '/testPost',
+      data: objectToSend,
+      }).then( function( response ){
+          console.log(response);
+          vm.getRecords();
+      });
+
+      vm.nameIn ='';
+      vm.locationIn='';
+    };
+
 vm.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-vm.allTheRecords = response;
-console.log( vm.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
-}]);
+  $http({
+    method: 'GET',
+    url: '/getRecords',
+  }).then( function success( response ){
+    vm.allTheRecords = response.data;
+    console.log( vm.allTheRecords );
+    // }), function myError( response ){
+    // console.log( response.statusText );
+    // };//end myError
+    });//end getRecords
+  };
+});
--- server/app.js
+++ server/app.js
@@ -1,38 +1,51 @@
 var express = require( 'express' );
-var app=express();
+var app = express();
 var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
-app.use( bodyParser.json() );
 var mongoose = require( 'mongoose' );
+
+
+app.use( express.static( 'public' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
+
 // 27017 is default mongo port
 mongoose.connect( 'localhost:27017/meanie' );
-var ourSchema = new  mongoose.Schema({
-name: String,
-location: String
+
+var ourSchema = mongoose.Schema({
+  name: String,
+  location: String
 });
-var ourModel = mongoose.model( 'ourModel', ourSchema );
+
+var ourModels = mongoose.model( 'ourModels', ourSchema );
+
 app.get( '/', function( req, res ){
-res.sendFile( path.resolve( 'public/index.html' ) );
+ res.sendFile( path.resolve( 'public/index.html' ) );
 });
+
 app.get( '/getRecords', function( req, res ){
 // get and send back all the things
-ourModel.find().then( function( data ){
-res.send( data );
+  ourModels.find().then( function( data ){
+    res.send( data );
+  });
 });
-});
+
 app.listen( 8080, 'localhost', function( req, res ){
-console.log( 'listening on 8080' );
-});
+  console.log( 'listening on 8080' );
+  });
+
+
 app.post( '/testPost', function( req, res ){
-console.log( 'req.body.name: ' + req.body.name );
+  console.log( 'req.body.name: ' + req.body );
 // retrieved the req.body
 // putting it into an object to be saved in the db
-var recordToAdd={
-name:req.body.name,
-location:req.body.location
-}
-// create new record
-var newRecord=ourModel( recordToAdd );
-newRecord.save();
-});
-app.use( express.static( 'public' ) );
+  // var recordToAdd = {
+  //     name:req.body.name,
+  //     location:req.body.location
+  //   };
+    // create new record
+    var newRecord = ourModels( req.body );
+    newRecord.save().then( function(){
+      res.sendStatus( 200 );
+    });
+  });
